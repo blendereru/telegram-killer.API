22dfb7dd386031219d99c4dcab98d8372636fc55
Sanzhar <sanzar30062000@gmail.com>
2026-03-24 12:59:54 +0600

    migrations due to LastReadAt, UpdatedAt properties

---
+++ telegram-killer.API/Migrations/20260324065739_AddLastReadAtDateToChatParticipant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace telegram_killer.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastReadAtDateToChatParticipant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Messages",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastReadAt",
+                table: "ChatParticipants",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReadAt",
+                table: "ChatParticipants");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Messages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
